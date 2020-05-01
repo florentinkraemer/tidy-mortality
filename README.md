d79acd5dc16073b1ac345a80e14bf9429f29ed4c
Florentin Krämer <25725752+florentinkraemer@users.noreply.github.com>
2020-05-01 17:37:46 +0200

    add graphs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tidy-mortality
-Clean and tidy mortality data for 2016 to 2020 from the Federal Statistical Office of Germany. The original dataset in .xlsx format is available at [this link](https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Sterbefaelle-Lebenserwartung/Tabellen/sonderauswertung-sterbefaelle.html?nn=209016). Note that data for 2019 and 2020 are preliminary and have not yet undergone plausibility checks.
+Clean and [tidy mortality data](data/) (.csv and .rds) for 2016 to 2020 from the Federal Statistical Office of Germany. The original dataset in .xlsx format is available at [this link](https://www.destatis.de/DE/Themen/Gesellschaft-Umwelt/Bevoelkerung/Sterbefaelle-Lebenserwartung/Tabellen/sonderauswertung-sterbefaelle.html?nn=209016). Note that data for 2019 and 2020 are preliminary and have not yet undergone plausibility checks.
 
 *Disclaimer*: I do not take responsibility for any errors either in the original dataset or in my cleaned-up version. Use with caution.
 
@@ -7,15 +7,26 @@ Clean and tidy mortality data for 2016 to 2020 from the Federal Statistical Offi
 
 The tidied data allow for different decompositions: by *federal state* and by *age group*. The raw dataset also contains mortality data for combinations of broader age groups (0-64 and 65+) and all federal states. In what follows, I first calculate the median number of deaths by federal state/age group for all years prior to 2020 (2016 to 2019). To provide a measure of how spread out these numbers are over the years, I also calculate the interquartile range, i.e. the range between the 25th and 75th percentile. Data for 2020 are then plotted as a smooth function using loess smoothing.
 
-## Excess Mortality By Federal State
+## By Federal State
+
+### Excess Mortality
 
 ![Excess Mortality by Federal State Relative to Median of 2016 to 2019](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_state_median.png)
-*Download* [pdf](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_state_median.pdf) or [png](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_state_median.png).
+Download as [pdf](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_state_median.pdf) or [png](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_state_median.png).
+
+### Cumulative Excess Deaths
+
+The following plot displays the cumulative number of deaths from January 1 until April 5, 2020 that can reasonably be considered "out of the ordinary", i.e. that fall outside the interquartile range. Note that we may have both positive and negative excess mortality. The calculation proceeds as follows: daily differences between the 25^th percentile and the number of deaths if the number of deaths is below the 25^th percentile, resp. daily differences between the 75^th percentile and the number of deaths if the number of deaths is above the 75^th percentile are summed up over the relevant date range. If the number of deaths falls within the interquartile range, excess deaths are recorded as zero.
+
+![Excess Mortality by Federal State Relative to Median of 2016 to 2019](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/cum_excess_mortality_state_median.png)
+Download as [pdf](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/cum_excess_mortality_state_median.pdf) or [png](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/cum_excess_mortality_state_median.png).
+
+### Average Ratio of Excess Deaths
 
 ## Excess Mortality By Age Group
 
 ![Excess Mortality by Age Group Relative to Median of 2016 to 2019](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_age_median.png)
-*Download* [pdf](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_age_median.pdf) or [png](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_age_median.png).
+Download as [pdf](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_age_median.pdf) or [png](https://github.com/florentinkraemer/tidy-mortality/blob/master/graphs/excess_mortality_age_median.png).
 
 
 
--- scripts/graphs.R
+++ scripts/graphs.R
@@ -3,11 +3,6 @@ library(hrbrthemes)
 
 # By State ----------------------------------------------------------------
 
-excess_deaths_byState <- states_days %>%
-  filter(year == 2020) %>%
-  group_by(federal_state) %>%
-  summarise(median.excess_mortality = sum(median.excess_mortality))
-
 ## Excess Mortality from All Causes by Federal State, Mean
 states_days %>%
   ggplot() +
@@ -29,31 +24,7 @@ states_days %>%
        y = "Ratio of 2020 deaths to mean 2016-2019 deaths",
        color = NULL,
        title = "Excess Mortality from All Causes by Federal State",
-       subtitle = "Baseline: mean value of years 2016-2019, shaded grey ribbon: one standard deviation \ndata for 2019 and 2020 preliminary",
-       caption = "Data: destatis, Analysis: @tfleauxk")
-
-## Excess Mortality from All Causes by Federal State, Mean
-states_days %>%
-  ggplot() +
-  geom_ribbon(aes(x = yday,
-                  ymin = lower_bound.mean.excess_mortality,
-                  ymax = upper_bound.mean.excess_mortality),
-              color = "grey30",
-              fill = "grey30") +
-  geom_hline(yintercept = 1) +
-  geom_smooth(data = subset(states_days, year == "2020"),
-              mapping = aes(x = yday,
-                            y = ratio.mean.excess_mortality,
-                            color = year),
-              se = TRUE) +
-  facet_wrap(~ federal_state) +
-  theme_ft_rc() +
-  scale_y_continuous(breaks = c(0.7, 1, 1.3)) +
-  labs(x = "Day of Year",
-       y = "Ratio of 2020 deaths to mean 2016-2019 deaths",
-       color = NULL,
-       title = "Excess Mortality from All Causes by Federal State",
-       subtitle = "Baseline: mean value of years 2016-2019, shaded grey ribbon: one standard deviation \ndata for 2019 and 2020 preliminary",
+       subtitle = "Relative to mean value of years 2016-2019. Shaded grey ribbon: one standard deviation. \nData for 2019 and 2020 preliminary. Details: https://tinyurl.com/yacmwu9d.",
        caption = "Data: Destatis, Analysis: Florentin Krämer")
 
 ggsave(filename = "excess_mortality_state_mean.pdf", path = "graphs/", device = cairo_pdf, 
@@ -83,13 +54,65 @@ states_days %>%
        y = "Ratio of 2020 deaths to median 2016-2019 deaths",
        color = NULL,
        title = "Excess Mortality from All Causes by Federal State",
-       subtitle = "Baseline: median value of years 2016-2019, shaded grey ribbon: interquartile range \ndata for 2019 and 2020 preliminary",
+       subtitle = "Relative to median value of years 2016-2019. Shaded grey ribbon: interquartile range. \nData for 2019 and 2020 preliminary. Details: https://tinyurl.com/yacmwu9d.",
        caption = "Data: Destatis, Analysis: Florentin Krämer")
 
 ggsave(filename = "excess_mortality_state_median.pdf", path = "graphs/", device = cairo_pdf, 
        width = 29.7, height = 21, units = "cm")
 
 ggsave(filename = "excess_mortality_state_median.png", path = "graphs/", type = "cairo-png", 
+       width = 29.7, height = 21, units = "cm", dpi = 300)
+
+## Cumulative Excess Deaths by Federal State, Median
+states_days %>%
+  filter(year == 2020) %>%
+  mutate(excess_deaths = case_when(n.deaths < p25.n.deaths ~ n.deaths - p25.n.deaths,
+                                   n.deaths > p75.n.deaths ~ n.deaths - p75.n.deaths,
+                                   n.deaths >= p25.n.deaths & n.deaths <= p75.n.deaths ~ 0)) %>%
+  group_by(federal_state) %>%
+  summarise(excess_deaths = sum(excess_deaths)) %>%
+  ggplot(aes(x = fct_reorder(as_factor(federal_state), excess_deaths),
+             y = excess_deaths)) +
+  geom_col() +
+  geom_hline(yintercept = 0, color = "steelblue") +
+  coord_flip() +
+  theme_ft_rc() +
+  labs(title = "Cumulative Excess Deaths by Federal State",
+       subtitle = "Relative to median value of years 2016-2019.\nJanuary 1, 2020 to April 5, 2020.",
+       x = NULL,
+       y = "Cumulative Number of Excess Deaths",
+       caption = "Data: Destatis, Analysis: Florentin Krämer")
+
+ggsave(filename = "cum_excess_mortality_state_median.pdf", path = "graphs/", device = cairo_pdf, 
+       width = 29.7, height = 21, units = "cm")
+
+ggsave(filename = "cum_excess_mortality_state_median.png", path = "graphs/", type = "cairo-png", 
+       width = 29.7, height = 21, units = "cm", dpi = 300)
+
+## Average Ratio of Excess Deaths by Federal State, Median
+states_days %>%
+  filter(year == 2020) %>%
+  mutate(ratio.excess_deaths = case_when(n.deaths < p25.n.deaths ~ (n.deaths + (n.deaths - p25.n.deaths)) / n.deaths,
+                                         n.deaths > p75.n.deaths ~ (n.deaths + (n.deaths - p75.n.deaths)) / n.deaths,
+                                         n.deaths >= p25.n.deaths & n.deaths <= p75.n.deaths ~ 1)) %>%
+  group_by(federal_state) %>%
+  summarise(ratio.excess_deaths = mean(ratio.excess_deaths)) %>%
+  ggplot(aes(x = fct_reorder(as_factor(federal_state), ratio.excess_deaths),
+             y = ratio.excess_deaths)) +
+  geom_point() +
+  geom_hline(yintercept = 1, color = "steelblue") +
+  coord_flip() +
+  theme_ft_rc() +
+  labs(title = "Average Ratio of Excess Deaths to Median Deaths by Federal State",
+       subtitle = "Relative to median value of years 2016-2019.\nJanuary 1, 2020 to April 5, 2020.",
+       x = NULL,
+       y = "Ratio of Excess Deaths to Median Deaths",
+       caption = "Data: Destatis, Analysis: Florentin Krämer")
+
+ggsave(filename = "avg_ratio_excess_mortality_state_median.pdf", path = "graphs/", device = cairo_pdf, 
+       width = 29.7, height = 21, units = "cm")
+
+ggsave(filename = "avg_ratio_xcess_mortality_state_median.png", path = "graphs/", type = "cairo-png", 
        width = 29.7, height = 21, units = "cm", dpi = 300)
 
 # By Age Group ------------------------------------------------------------
@@ -111,7 +134,7 @@ germany_ages %>%
   facet_wrap(~ age_category) +
   theme_ft_rc() +
   labs(title = "Excess Mortality from All Causes by Age Categories",
-       subtitle = "Baseline: mean value of years 2016-2019, shaded grey ribbon: one standard deviation \ndata for 2019 and 2020 preliminary",
+       subtitle = "Relative to mean value of years 2016-2019. Shaded grey ribbon: one standard deviation. \nData for 2019 and 2020 preliminary. Details: https://tinyurl.com/yacmwu9d.",
        x = "Date",
        y = "Ratio of 2020 deaths to mean 2016-2019 deaths",
        color = NULL,
@@ -144,10 +167,62 @@ germany_ages %>%
        x = "Date",
        y = "Ratio of 2020 deaths to median 2016-2019 deaths",
        color = NULL,
-       caption = "Data: destatis, Analysis: Florentin Krämer")
+       caption = "Data: Destatis, Analysis: Florentin Krämer")
 
 ggsave(filename = "excess_mortality_age_median.pdf", path = "graphs/", device = cairo_pdf, 
        width = 29.7, height = 21, units = "cm")
 
 ggsave(filename = "excess_mortality_age_median.png", path = "graphs/", type = "cairo-png", 
+       width = 29.7, height = 21, units = "cm", dpi = 300)
+
+## Cumulative Excess Deaths by Federal State, Median
+germany_ages %>%
+  filter(year == 2020) %>%
+  mutate(excess_deaths = case_when(n.deaths < p25.n.deaths ~ n.deaths - p25.n.deaths,
+                                   n.deaths > p75.n.deaths ~ n.deaths - p75.n.deaths,
+                                   n.deaths >= p25.n.deaths & n.deaths <= p75.n.deaths ~ 0)) %>%
+  group_by(age_category) %>%
+  summarise(excess_deaths = sum(excess_deaths)) %>%
+  ggplot(aes(x = fct_reorder(as_factor(age_category), excess_deaths),
+             y = excess_deaths)) +
+  geom_col() +
+  geom_hline(yintercept = 0, color = "steelblue") +
+  coord_flip() +
+  theme_ft_rc() +
+  labs(title = "Cumulative Excess Deaths by Age Group",
+       subtitle = "Relative to median value of years 2016-2019.\nJanuary 1, 2020 to April 5, 2020.",
+       x = NULL,
+       y = "Cumulative Number of Excess Deaths",
+       caption = "Data: Destatis, Analysis: Florentin Krämer")
+
+ggsave(filename = "cum_excess_mortality_age_median.pdf", path = "graphs/", device = cairo_pdf, 
+       width = 29.7, height = 21, units = "cm")
+
+ggsave(filename = "cum_excess_mortality_age_median.png", path = "graphs/", type = "cairo-png", 
+       width = 29.7, height = 21, units = "cm", dpi = 300)
+
+## Average Ratio of Excess Deaths by age Group, Median
+germany_ages %>%
+  filter(year == 2020) %>%
+  mutate(ratio.excess_deaths = case_when(n.deaths < p25.n.deaths ~ (n.deaths + (n.deaths - p25.n.deaths)) / n.deaths,
+                                         n.deaths > p75.n.deaths ~ (n.deaths + (n.deaths - p75.n.deaths)) / n.deaths,
+                                         n.deaths >= p25.n.deaths & n.deaths <= p75.n.deaths ~ 1)) %>%
+  group_by(age_category) %>%
+  summarise(ratio.excess_deaths = mean(ratio.excess_deaths)) %>%
+  ggplot(aes(x = fct_reorder(as_factor(age_category), ratio.excess_deaths),
+             y = ratio.excess_deaths)) +
+  geom_point() +
+  geom_hline(yintercept = 1, color = "steelblue") +
+  coord_flip() +
+  theme_ft_rc() +
+  labs(title = "Average Ratio of Excess Deaths to Median Deaths by Age Category",
+       subtitle = "Relative to median value of years 2016-2019.\nJanuary 1, 2020 to April 5, 2020.",
+       x = NULL,
+       y = "Ratio of Excess Deaths to Median Deaths",
+       caption = "Data: Destatis, Analysis: Florentin Krämer")
+
+ggsave(filename = "avg_ratio_excess_mortality_age_median.pdf", path = "graphs/", device = cairo_pdf, 
+       width = 29.7, height = 21, units = "cm")
+
+ggsave(filename = "avg_ratio_xcess_mortality_age_median.png", path = "graphs/", type = "cairo-png", 
        width = 29.7, height = 21, units = "cm", dpi = 300)
